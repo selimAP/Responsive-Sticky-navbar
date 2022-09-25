e18c3581abc713b35362f9049d159f4dff11dd02
selim <Selim3Boss@gmail.com>
2022-09-25 22:40:04 +0200

    makes sticky

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ document.querySelectorAll(".navlink").forEach(n => n.addEventListener("click", (
     lines.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+
+window.addEventListener("scroll", function(){
+    let header = this.document.querySelector("header");
+
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
